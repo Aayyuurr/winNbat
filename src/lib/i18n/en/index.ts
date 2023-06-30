969114245974e72b793c335b1ed93712bdaed267
Aayyuurr <abbasyanis4@gmail.com>
2023-06-30 17:07:22 +0200

    add margin top add autofocus add type submit button

---
+++ src/lib/i18n/en/index.ts
@@ -0,0 +1,15 @@
+import type { Translation } from '../i18n-types'
+
+const en = {
+ connexion:"Login/Sign up to winNbat",
+  VousAvezPasDeCompte:"Don't have an account?",
+  InscrivezVous:" Sign up",
+  SeConnecter:"Login",
+  Password:"Password",
+  Email:"Email",
+  Afficher:"Show",
+  Masquer:"Hide",
+  MotDePasseOublie:"Forgot password?",
+} satisfies Translation
+
+export default de
---
+++ src/lib/i18n/fr/index.ts
@@ -0,0 +1,15 @@
+import type { BaseTranslation } from '../i18n-types'
+
+const fr = {
+	connexion:"Connexion/inscription à winNbat",
+	VousAvezPasDeCompte:"Vous n'avez pas de compte?",
+	InscrivezVous:" Inscrivez-vous",
+	SeConnecter:"Se connecter",
+	Password:"Mot de passe",
+	Email:"Email",
+	Afficher:"Afficher",
+	Masquer:"Masquer",
+	MotDePasseOublie:"Mot de passe oublié?",
+} satisfies BaseTranslation
+
+export default fr
